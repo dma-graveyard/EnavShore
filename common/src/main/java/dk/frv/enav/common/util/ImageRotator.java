155f72985f228b9d846e0ef1c2dadce4856c9be3
Jacob Salomonsen <jsa@steria.dk>
2011-12-09 11:14:37 +0100

    Image rotator creates a sprite sheet

--- common/src/main/java/dk/frv/enav/common/util/ImageRotator.java
+++ common/src/main/java/dk/frv/enav/common/util/ImageRotator.java
@@ -21,40 +21,56 @@ public class ImageRotator {
 	 * 
 	 */
 	public void rotate() {
+		// Argument 0 target dir
 		String path = args[0];
 		System.out.println(path);
-		for(int i = 1; i < args.length ; i++) {
-			File file = new File(args[i]);
-			String[] fileName = file.getName().split("\\.");
-			try {
-				int interval = 5;
+		// Argument 1..n images to rotate
+		
+
+		int imageDimension = 32; // Quadratic image
+		int rotationInterval = 10;
+		
+		// empty buffered image used for clearing
+		BufferedImage clear = new BufferedImage(imageDimension, imageDimension, BufferedImage.TYPE_INT_ARGB);
+
+		// sprite sheet
+		int spriteHeight = imageDimension * 360 / rotationInterval;
+		int spriteWidth = (args.length - 1) * imageDimension;
+		BufferedImage sprite = new BufferedImage(spriteWidth, spriteHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		try {
+			for(int i = 1; i < args.length ; i++) {
+				File file = new File(args[i]);
 				int index = 1;
 				
+				int horizontalIndex = (i - 1) * imageDimension;
+				
 				Image image = ImageIO.read(file);
-				BufferedImage source = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+				BufferedImage source = new BufferedImage(imageDimension, imageDimension, BufferedImage.TYPE_INT_ARGB);
 				Graphics2D sourceGraphics = source.createGraphics();
 				sourceGraphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 				sourceGraphics.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 				sourceGraphics.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_DISABLE);
+				
+				// stamp the first image
 				sourceGraphics.drawImage(image, 0, 0, null);
+				sprite.setData(source.getData().createTranslatedChild(horizontalIndex, 0));
 				
-				BufferedImage clear = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-				
-				BufferedImage target = new BufferedImage(image.getWidth(null), image.getHeight(null)*(360/interval), BufferedImage.TYPE_INT_ARGB);
-				target.setData(source.getData());
-				
-				for (int j = 0; j < 359; j+=interval) {
+				for (int j = 0; j < 359; j+=rotationInterval) {
+					// clear the buffer
 					source.setData(clear.getData());
-					sourceGraphics.rotate(Math.toRadians(interval), image.getWidth(null)/2, image.getHeight(null)/2);
+					// and rotate
+					sourceGraphics.rotate(Math.toRadians(rotationInterval), image.getWidth(null)/2, image.getHeight(null)/2);
+					// stamp the next image
 					sourceGraphics.drawImage(image, 0, 0, null);
-					target.setData(source.getData().createTranslatedChild(0, index*32));
+					sprite.setData(source.getData().createTranslatedChild(horizontalIndex, index * imageDimension));
+					// update index for positioning
 					index++;
 				}
-				
-				ImageIO.write(target, "png", new File(path + "\\" + fileName[0] + ".png"));
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
+			ImageIO.write(sprite, "png", new File(path + "\\ships.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
