b78b16040eda3ee62925bdf4db4974bd72b095ad
Jens Tuxen <mail@jenstuxen.com>
2013-04-12 14:17:37 +0200

    service has optional provider handling, with default to DMI as metoc provider

--- core/src/main/java/dk/frv/enav/shore/core/services/metoc/MetocServiceBean.java
+++ core/src/main/java/dk/frv/enav/shore/core/services/metoc/MetocServiceBean.java
@@ -8,7 +8,9 @@ import javax.ejb.Stateless;
 import dk.frv.enav.common.xml.metoc.request.MetocForecastRequest;
 import dk.frv.enav.common.xml.metoc.request.MetocForecastRequestWp;
 import dk.frv.enav.common.xml.metoc.response.MetocForecastResponse;
+import dk.frv.enav.shore.core.metoc.DmiMetocClient;
 import dk.frv.enav.shore.core.metoc.MetocHttpInvoker;
+import dk.frv.enav.shore.core.metoc.MetocInvoker;
 import dk.frv.enav.shore.core.metoc.MetocInvokerException;
 import dk.frv.enav.shore.core.services.Errorcodes;
 import dk.frv.enav.shore.core.services.ServiceException;
@@ -48,7 +50,18 @@ public class MetocServiceBean implements MetocService {
 			request.getWaypoints().remove(0);
 		}
 
-		MetocHttpInvoker invoker = new MetocHttpInvoker(request);
+		String provider = request.getProvider().toLowerCase();
+		MetocInvoker invoker;
+		if (provider.equals("dmi")) {
+		     invoker = new DmiMetocClient(request);    
+		} else if (provider.equals("fco")) {
+		    invoker = new MetocHttpInvoker(request);
+		} else {
+		    invoker = new DmiMetocClient(request);
+		}
+		
+		//invoker = new MetocHttpInvoker(request);
+		
 
 		// Make the request
 		MetocForecastResponse res;
