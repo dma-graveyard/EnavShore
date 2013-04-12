c42d5b4ffe52f6d078182deb40655e48b95eef0d
Jens Tuxen <mail@jenstuxen.com>
2013-04-12 14:08:51 +0200

    to support DMI client webservice, DateUtils has been extended with a specific DMI date parsing format

--- common/src/main/java/dk/frv/enav/common/util/DateUtils.java
+++ common/src/main/java/dk/frv/enav/common/util/DateUtils.java
@@ -1,5 +1,6 @@
 package dk.frv.enav.common.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -12,6 +13,23 @@ public class DateUtils {
 		SimpleDateFormat iso8601gmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 		iso8601gmt.setTimeZone(tzGMT);
 		return iso8601gmt.format(date);
+	}
+	
+	public static String getISO8620SSSZZ(Date date) {
+	    SimpleDateFormat iso8601gmtz = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+	    iso8601gmtz.setTimeZone(tzGMT);
+	    return iso8601gmtz.format(date);
+	}
+	
+	public static Date getISO8620SSSZZ(String date)  {
+	    SimpleDateFormat iso8601gmtz = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+	    try {
+            return iso8601gmtz.parse(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
 	}
 
 }
